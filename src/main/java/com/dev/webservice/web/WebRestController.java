66440d0c3fc5413f25ab0434d62bd607ae88483e
jw <test@test.com>
2018-03-01 01:44:44 +0900

    1차 기능 구현

--- src/main/java/com/dev/webservice/web/WebRestController.java
+++ src/main/java/com/dev/webservice/web/WebRestController.java
@@ -1,13 +1,28 @@
 package com.dev.webservice.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dev.webservice.domain.posts.PostsRepository;
+import com.dev.webservice.dto.posts.PostsSaveRequestDto;
+
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+	
+	private PostsRepository postsRepository; // lombok의 @AllArgsConstructor를 이용해 @Autowired가 아닌 생성자로 주입.
 	
 	@GetMapping("/hello")
 	public String hello() {
 		return "hello";
+	}
+	
+	@PostMapping("/posts")
+	public void savePosts(@RequestBody PostsSaveRequestDto dto) { // @RequestBody을 이용해 HTTP 요청을 자바 객체로 매핑.
+		postsRepository.save(dto.toEntity());
 	}
 }
